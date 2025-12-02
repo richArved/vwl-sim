136c7191fb984a7b97a0bafc307078316f5fb247
muayarved@gmail.com <arved@MacBook-Pro-von-Arved.local>
2025-12-02 13:56:00 +0100

    fix pathes 2

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'  // <--- NEU
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <--- NEU: Hier hinzufügen
+    tailwindcss(),
   ],
+  base: '/vwl-sim/', // <--- DIESE ZEILE HINZUFÜGEN (Name deines Repos)
 })
